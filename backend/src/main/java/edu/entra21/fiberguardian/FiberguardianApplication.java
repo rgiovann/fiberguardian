22f39f5d55a32c3b1c0173ffef86b65f5ef0b83c
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-06-12 00:56:43 -0300

    Spring Initializer

---
+++ backend/src/main/java/edu/entra21/fiberguardian/FiberguardianApplication.java
@@ -0,0 +1,13 @@
+package edu.entra21.fiberguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiberguardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiberguardianApplication.class, args);
+	}
+
+}
