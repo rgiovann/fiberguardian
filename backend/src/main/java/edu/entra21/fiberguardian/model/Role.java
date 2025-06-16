6403e51b8fecb685a5ce81c8adc6985490d1e1dd
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-06-16 17:06:33 -0300

    Adicionando entity Usuario e enum Role em ../model

---
+++ backend/src/main/java/edu/entra21/fiberguardian/model/Role.java
@@ -0,0 +1,8 @@
+package edu.entra21.fiberguardian.model;
+
+public enum Role {
+    ADMIN,
+    USUARIO,
+    LABORATORIO,
+    ENGENHARIA
+}
---
+++ backend/src/main/java/edu/entra21/fiberguardian/model/Usuario.java
@@ -0,0 +1,55 @@
+package edu.entra21.fiberguardian.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 50, message = "Nome deve ter até 50 caracteres")
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    @Size(max = 50, message = "Email deve ter até 50 caracteres")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(max = 50, message = "Senha deve ter até 50 caracteres")
+    @Column(name = "senha", nullable = false)
+    @ToString.Exclude
+    private String senha;
+
+    @NotNull(message = "Ativo é obrigatório")
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo = true;
+
+    @NotNull(message = "Role é obrigatória")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.USUARIO;
+
+    public Usuario() {
+    }
+}
