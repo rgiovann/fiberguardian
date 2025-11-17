daa4e3f772deb0ad2916ad76ce7eedef2b621c84
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-11-17 18:21:52 -0300

    Adicionado arquivos de tela consulta testes reprovados e tabela de testes reprovados

--- frontend/src/main/webapp/assets/js/fiberguardian_core.js
+++ frontend/src/main/webapp/assets/js/fiberguardian_core.js
@@ -36,11 +36,13 @@
             ],
             'tela_consulta_laboratorio.html': [
                 'assets/js/fiberguardian_utils.js',
+                'assets/js/fiberguardian_core.js',
                 'assets/js/tela_consulta_laboratorio.js',
             ],
-            'tela_teste_reprovados.html': [
+            'tela_consulta_testes_reprovados.html': [
                 'assets/js/fiberguardian_utils.js',
-                'assets/js/tela_teste_reprovados.js',
+                'assets/js/fiberguardian_core.js',
+                'assets/js/tela_consulta_testes_reprovados.js',
             ],
             'tela_cadastro_parecer_engenharia.html': [
                 'assets/js/fiberguardian_utils.js',
--- inicia_http_server_caddy.bat
+++ inicia_http_server_caddy.bat
@@ -9,6 +9,16 @@ set "WEBAPP_DIR=%BASE_DIR%\src\main\webapp"
 rem Muda para o diretório dos certificados e do Caddyfile
 cd /d "%CERT_DIR%"
 
+rem Verifica se a porta 8080 está ocupada
+netstat -ano | findstr :8080 >nul
+if not errorlevel 1 (
+    echo [ERRO] Porta 8080 ja esta em uso. Feche o processo e tente novamente.
+    pause
+    exit /b 1
+) else (
+    echo [INFO] Porta 8080 vazia! Continuando configuracao...
+)
+
 rem Inicia http-server com SSL em nova janela (em background)
 echo Iniciando http-server com SSL em nova janela...
 start "HTTP Server" cmd /k npx http-server "%WEBAPP_DIR%" --ssl --cert "%CERT_DIR%\cert.pem" --key "%CERT_DIR%\key.pem"
